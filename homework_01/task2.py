19e1e6341116ffb4cf09af73148413fcc8d0064f
AndriiDorokhov <dorokhov.andrii@gmail.com>
2023-06-30 01:53:18 +0200

    add new folder

---
+++ homework_01/task2.py
@@ -0,0 +1,10 @@
+
+#The print function should output the days of the week to the terminal and replace the spaces
+# between the words with a non-"--" sign
+#Sunday--Monday--Tuesday--Wednesday--Thursday--Friday--Saturday
+print("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", sep="--")
+
+#The print function should output the days of the week to the terminal and replace the spaces
+# between the words with a non-"--" sign. And also add the line "--Days of the week" at the end of the line
+#Sunday--Monday--Tuesday--Wednesday--Thursday--Friday--Saturday--Days of the week
+print("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", sep="--",end="--Days of the week")
