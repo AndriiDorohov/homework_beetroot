b77ab047873c7139b13e7c6d66606ab66bc680be
AndriiDorokhov <dorokhov.andrii@gmail.com>
2023-07-11 18:33:05 +0200

    change function+return

--- homework_04/task02.py
+++ homework_04/task02.py
@@ -10,8 +10,7 @@
 def user_details():
     global user_name, user_age
     user_list = input("Enter your name and age separated by a space").split()
-    user_name = user_list[0]
-    user_age = int(user_list[1])
+    return (user_list[0], int(user_list[1]))
 
-user_details()
+user_name, user_age = user_details()
 print(f"Hello {user_name}, on your next birthday you’ll be {user_age + 1} years")
