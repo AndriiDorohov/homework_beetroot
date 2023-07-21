e5a6da46e2b644a1dd010a2070eda93b8613faf1
AndriiDorokhov <dorokhov.andrii@gmail.com>
2023-07-21 21:21:42 +0200

    add hw9

---
+++ homework_09/task02.py
@@ -0,0 +1,23 @@
+# Task 2
+
+# Write a function that takes in two numbers from
+# the user via input(), call the numbers a and b,
+# and then returns the value of squared a divided by
+# b, construct a try-except block which raises an exception
+# if the two values given by the input function were not numbers,
+# and if value b was zero (cannot divide by zero).
+
+def calc_val(a, b, *arg):
+    return a * a / b
+
+str_a = input("Input a & b with space: ").split()
+
+try:
+     int_b = [int(x) for x in str_a]
+     if int_b[1] == 0:
+          print('ZeroDivisionError')
+     else:
+          print(calc_val(*int_b))
+
+except ValueError:
+     print('You entered a non-numeric value')
