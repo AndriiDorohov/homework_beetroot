05683923f731f0cd87cff340b625d2f7c7971713
AndriiDorokhov <dorokhov.andrii@gmail.com>
2023-07-31 01:31:36 +0200

    add funcion

--- homework_11/task03.py
+++ homework_11/task03.py
@@ -15,19 +15,19 @@
 
 def choose_func(nums: list, func1, func2):
 		if min(nums) >= 0:
-						return func1(min(nums))
+				return func1(nums)
 		else:
-						return func2(min(nums))
+				return func2(nums)
 
-def func_1(arg):
-		return arg + 1
+def square_nums(nums):
+		return [num ** 2 for num in nums]
 
-def func_2(arg):
-		return arg + arg
+def remove_negatives(nums):
+		return [num for num in nums if num > 0]
 
 nums1 = [1, 2, 3, 4, 5]
 nums2 = [1, -2, 3, -4, 5]
-result = choose_func(nums1, func_1, func_2)
+result = choose_func(nums1, square_nums, remove_negatives)
 print(result)
-result = choose_func(nums2, func_1, func_2)
+result = choose_func(nums2, square_nums, remove_negatives)
 print(result)
