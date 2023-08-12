d0392b6c003e5b263ed1c11795e82de6a296d078
AndriiDorokhov <dorokhov.andrii@gmail.com>
2023-08-12 02:13:24 +0200

    add hw16

---
+++ homework_16/task01.py
@@ -0,0 +1,10 @@
+# Task 1
+
+# Create a class method named `validate`, which should be called from
+# the `__init__` method to validate parameter email, passed to the constructor.
+# The logic inside the `validate` method could be to check if the passed email
+# parameter is a valid email string.
+
+# Email validations:
+# https://help.xmatters.com/ondemand/trial/valid_email_format.htm
+# https://en.wikipedia.org/wiki/Email_address
