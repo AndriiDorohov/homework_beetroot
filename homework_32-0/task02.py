6e2fae07ff14be6b95fa9c0b8f6552eb3e9d0a8f
Andrii Dorokhov <wedpositive@gmail.com>
2023-09-26 00:17:11 +0200

    Delete homework_32-0 directory

--- homework_32-0/task02.py
+++
@@ -1,9 +0,0 @@
-# Task 2
-
-# Requests using concurrent and multiprocessing libraries
-
-# Download all comments from a subreddit of your choice using
-# URL: https://api.pushshift.io/reddit/comment/search/ .
-
-# As a result, store all comments in chronological order in JSON and dump it to a file.
-# For this task use concurrent and multiprocessing libraries for making requests to Reddit API.
--- homework_32-0/task03.py
+++
@@ -1,6 +0,0 @@
-# Task 3
-
-# Echo server with threading
-
-# Create a socket echo server that handles each connection
-# using the multiprocessing library.
