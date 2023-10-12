68ca01219610b6934743a4f3b5354f04b20a259a
AndriiDorohov <dorokhov.andrii@gmail.com>
2023-10-13 00:20:16 +0200

    upgrade

--- homework_36-0/notes_app/notes_app/settings.py
+++ homework_36-0/notes_app/notes_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "notes",
 ]
 
 MIDDLEWARE = [
@@ -77,14 +78,11 @@ WSGI_APPLICATION = "notes_app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "my_notes_db",
-        "USER": "my_db_user",
-        "PASSWORD": "my_password",
-        "HOST": "localhost",
-        "PORT": "5432",
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
